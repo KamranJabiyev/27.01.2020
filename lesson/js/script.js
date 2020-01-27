07d11d5e487c58795342cba1778832a8d4907da3
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-01-27 21:42:58 +0400

    Add files via upload

---
+++ lesson/js/script.js
@@ -0,0 +1,70 @@
+// console.log("Salam Elnur qaga")
+// var x=5;
+// var y=6.5;
+// var name='Elnur';
+// x="Kamran";
+
+// var y=95;
+
+// let z=15;
+// z=20;
+// let Z=30;
+// const d=100;
+// let w="Elnur Asadov";
+// console.log(d);
+
+//camel case
+// let userLoginParol="11111";
+
+//Pascal case
+// let UserLoginParol="asdfeth";
+
+//Snake case
+// let user_login_parol="asdfgh";
+
+//Kebab case
+
+
+
+//Aritmatic operators
+
+// let number=1000000;
+
+// let power=0;
+
+// console.log(number**power);
+
+// let x=15;
+// let y=10;
+
+// console.log(15%20)
+
+// let x=5;
+// x--;
+// x++;
+// x++;
+// x--;
+// console.log(x);
+
+
+//Assignment operators
+// let a=10;
+// a=11;
+
+// a**=2;
+// console.log(a)
+
+// let name="Elnur";
+// let surname="Asadov";
+// let x=10;
+// let word="100";
+// let z=null;
+
+// // console.log(name+" "+surname);
+// console.log(z);
+
+let a=50;
+let b=100;
+
+console.log(a)
+console.log(b)
